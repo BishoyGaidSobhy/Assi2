4e8c331f2be6453c75aff9d4ac0c666108572635
BishoyGaidSobhy <104456293+BishoyGaidSobhy@users.noreply.github.com>
2022-05-11 00:35:42 +0200

    Add files via upload

---
+++ drag_drop.js
@@ -0,0 +1,30 @@
+function oo7(){
+    let items=document.getElementById("textVal").value;
+    let count = 0;
+    let para=document.createElement("p");
+    para.setAttribute("id", count++);
+    para.setAttribute("draggable", "true");
+    para.innerText=items;
+    let y=document.getElementById("inProgresstoo");
+    y.appendChild(para);
+    //document.body.appendChild(y)
+    para.addEventListener("dragstart", function (event) {
+        event.dataTransfer.setData("text", this.id);
+      });
+    
+
+let boxs = document.getElementsByClassName("tasksSection");
+
+for (let i = 0; i < boxs.length; i++) {
+    boxs[i].addEventListener("dragover", feelDrag);
+    boxs[i].addEventListener("drop", dropAndADD);
+  }
+}
+function feelDrag(event) {
+    event.preventDefault();
+  }
+
+function dropAndADD(event) {
+    let dragedData = event.dataTransfer.getData("text");
+    this.appendChild(document.getElementById(dragedData));
+  }
